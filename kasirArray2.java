ebf6580698edfc7858993d5da09bfa5494b0c99a
Candrasholeh <candradion30@gmail.com>
2024-11-26 10:40:36 +0700

    oy

--- kasirArray2.java
+++ kasirArray2.java
@@ -11,18 +11,18 @@ public class kasirArray2 {
 
         System.out.println("Daftar menu makanan");
         for(int i = 0; i < makanan.length; i++){
-            System.out.println("["+i+"]- "+makanan[i]+" - Rp"+hargaMakanan[i]);
+            System.out.println("["+(i+1)+"]- "+makanan[i]+" - Rp"+hargaMakanan[i]);
         }
 
         System.out.print("Pilih makanan :");
         int pilihMakanan = input.nextInt();
 
         System.out.print("Beli Berapa? ");
-        jumlahMakanan[pilihMakanan=1]= input.nextInt();
+        jumlahMakanan[pilihMakanan-1]= input.nextInt();
 
-        double total = hargaMakanan[pilihMakanan=1] * jumlahMakanan[pilihMakanan=1];
+        double total = hargaMakanan[pilihMakanan-1] * jumlahMakanan[pilihMakanan-1];
 
-        System.out.println("Anda memesan :" +makanan[pilihMakanan=1]+" "+jumlahMakanan[pilihMakanan=1]+" Porsi");
+        System.out.println("Anda memesan :" +makanan[pilihMakanan-1]+" "+jumlahMakanan[pilihMakanan-1]+" Porsi");
         System.out.println("biaya Total = "+total);
 
         input.close();
